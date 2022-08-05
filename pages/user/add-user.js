d05005eb815fc83456d691c3e809023212c50b8e
techHim <faithsacredoo3@gmail.com>
2022-08-05 22:09:30 +0800

    first nexxtjs

---
+++ pages/user/add-user.js
@@ -0,0 +1,22 @@
+
+import { useState } from 'react';
+import axios from 'axios';
+import { async } from '@firebase/util';
+
+export default function AddUser() {
+    const [name, setName] = useState('')
+    return (
+        <div>
+            User Form Input
+
+            <input type="text" placeholder="Add name" onChange={(e) => { setName(e.target.value) }} />
+
+            <span>{name}</span>
+
+            <button onClick={async() => {
+                const user = await axios.post('http://localhost:3000/api/sample', {name, description: 'description'})
+                console.log(user);
+            }}>Add</button>
+        </div>
+    )
+}
---
+++ pages/user/index.js
@@ -0,0 +1,7 @@
+export default function Users() {
+    return (
+        <div>
+            <h1>Users table</h1>
+        </div> 
+    )
+}
